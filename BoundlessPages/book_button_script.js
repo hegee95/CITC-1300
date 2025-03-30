31364536fdd2a73a7576b2371d8a65360f2fc77f
Hannah <hegee@my.chattanoogastate.edu>
2025-03-30 14:46:21 -0500

    more css and html changes, getting formatting consistent

--- BoundlessPages/book_button_script.js
+++ BoundlessPages/book_button_script.js
@@ -2,17 +2,22 @@
 function toggleLinks() {
     const links = document.getElementById("book-links");
     const button = document.querySelector(".book-button");
-
+  
     if (!button.classList.contains("open")) {
-        button.classList.add("flipping");
-
-        setTimeout(() => {
-            button.classList.remove("flipping");
-            button.classList.add("open");
-        }, 300);
+      button.classList.add("flipping");
+  
+      setTimeout(() => {
+        button.classList.remove("flipping");
+        button.classList.add("open");
+      }, 300);
     } else {
-        button.classList.remove("open");
+      button.classList.remove("open");
     }
-
+  
     links.style.display = links.style.display === "block" ? "none" : "block";
-}
+  }
+  
+  function toggleSynopsis(bookContainer) {
+    const synopsis = bookContainer.querySelector(".synopsis");
+    synopsis.style.display = synopsis.style.display === "block" ? "none" : "block";
+  }
