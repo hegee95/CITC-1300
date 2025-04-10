ee693201de8634d53fefab248b3bf66ae47c033a
Hannah <hegee@my.chattanoogastate.edu>
2025-04-09 20:46:15 -0500

    Added further library shelf functionality

---
+++ BoundlessPages/js/bookshelf_interaction.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const books = document.querySelectorAll('.book');
+
+    books.forEach(book => {
+        book.addEventListener('mousedown', (e) => {
+            let shiftX = e.clientX - book.getBoundingClientRect().left;
+            let shiftY = e.clientY - book.getBoundingClientRect().top;
+
+            const moveAt = (pageX, pageY) => {
+                book.style.left = pageX - shiftX + 'px';
+                book.style.top = pageY - shiftY + 'px';
+            };
+
+            const onMouseMove = (e) => {
+                moveAt(e.pageX, e.pageY);
+            };
+
+            document.addEventListener('mousemove', onMouseMove);
+
+            book.addEventListener('mouseup', () => {
+                document.removeEventListener('mousemove', onMouseMove);
+                book.onmouseup = null;
+            });
+        });
+
+        book.ondragstart = () => false; // Prevent default drag behavior
+    });
+});
